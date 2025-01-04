b307a4bfeef6e7197be7242c222caa6f76ae7d28
Jennifer Adeola Akinboade <110998071+AdeolaAkinboade@users.noreply.github.com>
2025-01-04 13:52:51 +0200

    Add files via upload

---
+++ Code Testing.py
@@ -0,0 +1,22 @@
+password = input("Enter Password")
+
+while password != 'pass123':
+    password = input("Enter Password")
+
+print("Password was correct:)")
+
+
+#anything outside a loop is executed only once in Python
+#everything in a loop will be executed forever
+#if the while loop conditions have been met,
+#then it will stop the loop and print 'Password was correct'
+
+
+
+
+
+
+#while True:
+ #   print("Hello")
+    #will keep printing out hello forever until you stop it
+    #nb the outputs dont get printed out without the output  function
