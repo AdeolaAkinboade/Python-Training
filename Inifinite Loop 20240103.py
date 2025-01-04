3222f1863dbaca4cc864b6b962d06fcdc7a46b3a
Jennifer Adeola Akinboade <110998071+AdeolaAkinboade@users.noreply.github.com>
2025-01-04 13:49:56 +0200

    Add files via upload

---
+++ Inifinite Loop 20240103.py
@@ -0,0 +1,30 @@
+#Exercise1 Fix the bugs
+
+#while True:
+ #   print("Hello")
+#The while loop didnt have the ":" colon
+#added the colon
+#no indentation
+#added indentation
+
+
+#Exercise2
+
+#greeting = ("hello")
+
+#print(greeting.upper())
+
+#changed upper(greeting) to greeting.upper() for method to work
+
+
+#Exercise3
+
+
+user_prompt = "Enter the country:"
+
+countries = []
+
+while True:
+    country = input(user_prompt)
+    countries.append(country)
+    print(countries)
