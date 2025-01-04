d9c3e3eff5d52784a504e7a89bf65b2fe9c1c3da
Jennifer Adeola Akinboade <110998071+AdeolaAkinboade@users.noreply.github.com>
2025-01-04 13:52:08 +0200

    Add files via upload

---
+++ Coding Testing(2).py
@@ -0,0 +1,7 @@
+x = 1
+
+while x <=6:
+    print(x)
+    x = x + 1 #this equation is dynamic so it'll keep adding one to x until it reaches 6
+             #once 6 has been reached then the value is false so the while loop stops
+    #if we left it as x = 1 while x <=6: print(x)  it will read the variable one and forever print in the while loop cause its a true statement
