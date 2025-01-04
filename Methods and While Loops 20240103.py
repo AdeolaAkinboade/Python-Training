2cf2667ab69945ada650031fa1030e2337074c5e
Jennifer Adeola Akinboade <110998071+AdeolaAkinboade@users.noreply.github.com>
2025-01-04 13:51:40 +0200

    Add files via upload

---
+++ Methods and While Loops 20240103.py
@@ -0,0 +1,20 @@
+#Batch Operations in Python
+user_prompt = 'Enter too dooo:'
+
+while True:
+    todo = input(user_prompt)
+    print(todo)
+    print("Next.........")
+
+    #so with the while loop,python will check if the
+    #while loop condition is true
+    #then it will execute the next line
+    #execute print(todo) and print(next)
+    # then goes back to see if the while loop is true and
+    ##goes through the todo line again
+    #this has four loops in it
+
+    #Instead of writing a custom-made condition that makes the while loop true
+    #We can use while True(python built in prompt)
+    #Intially, we have the while condition say "While 2>1'
+    #True = Boolean and its kind of like a string
