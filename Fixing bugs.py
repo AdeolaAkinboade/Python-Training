fab082dfaafa06cf4ff15af9f7950a00c37ea818
Jennifer Adeola Akinboade <110998071+AdeolaAkinboade@users.noreply.github.com>
2025-01-04 13:51:09 +0200

    Add files via upload

---
+++ Fixing bugs.py
@@ -0,0 +1,27 @@
+#Fixing Bugs
+
+answers = ['Yes','No','Yes','No','No']
+
+#error said [ was never closed
+#added bracket as solution
+
+my_answer = input("What is your  name?")
+answers = ['Yes','No','Yes','No','No',my_answer]
+
+#says that I forgot to put a comma
+#added inverted commas to my code
+
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes' 'No',my_answer]
+
+#says that I forgot to put a comma
+#added a comma between variable and string
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+#object is not subscriptable was given as an error
+#means I am using square brackets on an object that doesnt support indexing
+#changed it to ()
+
